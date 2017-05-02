9c52867c845f4cee966516e0132511b322954242
Michael Stout <stout@uselessopinions.com>
2017-05-01 21:27:18 -0400

    Mushroom damage states in

---
+++ Assets/Scenes/Mushroom.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mushroom : MonoBehaviour 
+{
+	public Sprite[] damageStates = null;
+
+	private SpriteRenderer spriteRenderer = null;
+	public int currentDamageState = 0;
+
+	void Awake () 
+	{
+		spriteRenderer = GetComponent<SpriteRenderer>();
+		spriteRenderer.sprite = damageStates[0];
+	}//Awake
+	
+	void Update () 
+	{
+		if (currentDamageState < damageStates.Length)
+		{
+			spriteRenderer.sprite = damageStates[currentDamageState];
+		}//if
+		else
+		{
+			Destroy(gameObject);
+		}//else
+	}//Update
+
+}//Mushroom
