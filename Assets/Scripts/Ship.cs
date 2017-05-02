549bc4a00816d6cf6dcff28a4af89564e11ee93e
Michael Stout <stout@uselessopinions.com>
2017-05-01 21:47:58 -0400

    SHip movement within bounding box

---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour 
+{
+
+	//public float maxY = -2.5f;
+	public float moveSpeed = 10.0f;
+
+	public BoxCollider2D shipBounds = null;
+
+	private float inputX = 0;
+	private float inputY = 0;
+
+	private Rigidbody2D rb2d = null;
+
+	// Use this for initialization
+	void Awake () 
+	{
+		//rb2d = GetComponent<Rigidbody2D>();
+
+	}//Awake
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		inputX = Input.GetAxisRaw("Horizontal");
+		inputY = Input.GetAxisRaw("Vertical");
+	}//Update
+
+	void FixedUpdate()
+	{
+		//Vector2 oldPos = (Vector2)transform.position;
+
+		float xVel = inputX * moveSpeed * Time.deltaTime;
+		float yVel = inputY * moveSpeed * Time.deltaTime;
+		Vector2 moveVec = new Vector2(xVel, yVel);
+
+		if (shipBounds.bounds.Contains((Vector2)transform.position + moveVec))
+		{
+			transform.Translate(moveVec);
+		}//if
+
+
+	}//FixedUpdate
+}//Ship
